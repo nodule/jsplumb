e9308f951229cfb096b39740709b348b9329ba21
Rob Halff <rob.halff@gmail.com>
2016-03-21 00:50:53 +0100

    update

---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = {
+  addEndpoint: require('./addEndpoint'),
+  connect: require('./connect'),
+  detach: require('./detach'),
+  draggable: require('./draggable'),
+  getConnections: require('./getConnections'),
+  getContainer: require('./getContainer'),
+  getInstance: require('./getInstance'),
+  importDefaults: require('./importDefaults'),
+  recalculateOffsets: require('./recalculateOffsets'),
+  registerEndpointTypes: require('./registerEndpointTypes'),
+  remove: require('./remove'),
+  removeAllEndpoints: require('./removeAllEndpoints'),
+  repaint: require('./repaint'),
+  repaintEverything : ##repaintEverything ##,
+  reset: require('./reset'),
+  setContainer: require('./setContainer'),
+  setSuspendDrawing: require('./setSuspendDrawing')
+}
